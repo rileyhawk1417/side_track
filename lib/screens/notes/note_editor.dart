1ce3fa4afd3ddde3bc3a7b34da537e449288525f
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-09-19 00:11:06 +0200

    chore: add riverpod_annotation & refactor code
    - riverpod_annotation allows you to create providers on the fly.
    - Used it in the editor section, to determine which icon to show.

--- lib/screens/notes/note_editor.dart
+++ lib/screens/notes/note_editor.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:side_track/hive/notes/notes_model.dart';
+part 'note_editor.g.dart';
 
 List<MobileToolbarItem> mobileToolBar = [
   textDecorationMobileToolbarItem,
@@ -16,6 +18,37 @@ List<MobileToolbarItem> mobileToolBar = [
   dividerMobileToolbarItem,
   codeMobileToolbarItem,
 ];
+
+@riverpod
+class Editable extends _$Editable {
+  @override
+  bool build() => false;
+
+  void editable() => state = !state;
+}
+
+class AppBarButton extends StatelessWidget {
+  const AppBarButton(
+      {super.key, required this.onPressedFunc, required this.icon});
+  final void Function() onPressedFunc;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(150),
+        child: SizedBox(
+          height: 120.0,
+          width: 55.0,
+          child: MaterialButton(
+            onPressed: () => onPressedFunc,
+            child: Icon(icon),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class NoteEditor extends ConsumerWidget {
   final Map<String, dynamic> noteData;
@@ -33,6 +66,7 @@ class NoteEditor extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final editorScrollController = ScrollController();
+    var _editable = ref.watch(editableProvider);
     final _encodedNote =
         ref.watch(notesController).getJsonNote(noteDate, noteListIndex);
     //TODO Test save method, then try read from it.
@@ -43,29 +77,60 @@ class NoteEditor extends ConsumerWidget {
         ),
       ),
     );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(noteTitle),
-        actions: [
-          MaterialButton(
-            onPressed: () {},
-            child: const Icon(Icons.done),
-          )
-        ],
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: AppFlowyEditor(
-              editable: true,
-              editorStyle: const EditorStyle.mobile(),
-              editorState: editorState,
-              scrollController: editorScrollController,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(noteTitle),
+          actions: [
+            _editable
+                ? Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(150),
+                      child: SizedBox(
+                        height: 120.0,
+                        width: 55.0,
+                        child: MaterialButton(
+                          onPressed: () =>
+                              ref.read(editableProvider.notifier).editable(),
+                          child: const Icon(Icons.done),
+                        ),
+                      ),
+                    ),
+                  )
+                : Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(150),
+                      child: SizedBox(
+                        height: 120.0,
+                        width: 55.0,
+                        child: MaterialButton(
+                          onPressed: () =>
+                              ref.read(editableProvider.notifier).editable(),
+                          child: const Icon(Icons.edit),
+                        ),
+                      ),
+                    ),
+                  ),
+            const SizedBox(
+              height: 10,
+              width: 10,
             ),
-          ),
-          MobileToolbar(editorState: editorState, toolbarItems: mobileToolBar)
-        ],
+          ],
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: AppFlowyEditor(
+                editable: _editable,
+                editorStyle: const EditorStyle.mobile(),
+                editorState: editorState,
+                scrollController: editorScrollController,
+              ),
+            ),
+            MobileToolbar(editorState: editorState, toolbarItems: mobileToolBar)
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screens/notes/note_editor.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_editor.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$editableHash() => r'42f60fa22cfd57fad61ff354ec63feee1d59bf0b';
+
+/// See also [Editable].
+@ProviderFor(Editable)
+final editableProvider = AutoDisposeNotifierProvider<Editable, bool>.internal(
+  Editable.new,
+  name: r'editableProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$editableHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Editable = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
